7b259430801624cfacdba527bf2b612c2682327b
ROBERT Antoine <arobert@silene-habitat.com>
2017-11-05 22:40:09 +0100

    Init Tuto

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ app.config([
 			url: '/home',
 			templateUrl: '/home.html',
 			controller: 'MainCtrl'
+		})
+		.state('posts', {
+			url: '/posts/{id}',
+			templateUrl: '/posts.html',
+			controller: 'PostsCtrl'
 		});
 
 		$urlRouterProvider.otherwise('home');
@@ -34,7 +39,11 @@ app.controller('MainCtrl', [
 			$scope.posts.push({
 				title: $scope.title,
 				link: $scope.link,
-				upvotes: 0
+				upvotes: 0,
+				comments: [
+				{author: 'Joe', body: 'Cool post!', upvotes: 0},
+				{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+				]
 			});
 			$scope.title = '';
 			$scope.link = '';
@@ -42,5 +51,25 @@ app.controller('MainCtrl', [
 
 		$scope.incrementUpvotes = function(post) {
 			post.upvotes += 1;
+		};
+	}]);
+
+app.controller('PostsCtrl', [
+	'$scope',
+	'$stateParams',
+	'posts',
+	function($scope, $stateParams, posts){
+		
+		$scope.post = posts.posts[$stateParams.id];
+
+
+		$scope.addComment = function(){
+			if($scope.body === '') { return; }
+			$scope.post.comments.push({
+				body: $scope.body,
+				author: 'user',
+				upvotes: 0
+			});
+			$scope.body = '';
 		};
 	}]);
